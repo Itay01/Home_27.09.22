5c7e997299c348683e90674f27e4307f2abaeebc
Itay01 <104745527+Itay01@users.noreply.github.com>
2022-09-27 22:28:44 +0300

    Add assignment 18

---
+++ 22.07.22-73,19/22.07.22-73,19/Program.cs
@@ -0,0 +1,26 @@
+﻿class Assgnment_19
+{
+    public static void Main()
+    {
+        int digit, num, m;
+        Console.WriteLine("Enter a digit:");
+        digit = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter a two digits number:");
+        num = int.Parse(Console.ReadLine());
+        m = 0;
+        if (num / 10 == digit)
+            m++;
+        if (num % 10 == digit)
+            m++;
+        Console.WriteLine("m = {0}", m);
+    }
+}
+
+
+//output:
+//    digit = 5, num = 55 | m = 2.
+//    digit = 3, num = 34 | m = 1.
+//    e.x: m = 0 | digit = 1, num = 20.
+//    The purpose of this program is to check if the digit is equal to:
+//      1. The number divided by 10.
+//      2. The left digit.
