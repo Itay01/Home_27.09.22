032cb43905ace13d19413a283975ca540a0e6c6e
Itay01 <104745527+Itay01@users.noreply.github.com>
2022-09-27 22:30:30 +0300

    Add assignment 20

---
+++ 27.09.22-73,20/27.09.22-73,20/Program.cs
@@ -0,0 +1,24 @@
+﻿class Assgnment_20
+{
+    public static void Main()
+    {
+        int num, max, t;
+        Console.WriteLine("Enter a three digits number:");
+        num = int.Parse(Console.ReadLine());
+        max = num % 10;
+        t = num / 10 % 10;
+        if (t > max)
+            max = t;
+        t = num / 100;
+        if (t > max)
+            max = t;
+        Console.WriteLine("the digit is {0}", max);
+    }
+}
+
+
+//output:
+//    num = 692 | max = 9.
+//    num = 811 | max = 8.
+//    e.x: max = 9 | num = 592.
+//    The purpose this program is to find the largest digit in the number.
