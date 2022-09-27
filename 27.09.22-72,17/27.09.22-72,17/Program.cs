19f7f1d2e09746e5f8743e189b2d4a6e0111eb37
Itay01 <104745527+Itay01@users.noreply.github.com>
2022-09-27 22:26:42 +0300

    Initial commit

---
+++ 27.09.22-72,17/27.09.22-72,17/Program.cs
@@ -0,0 +1,25 @@
+﻿class Assignment_17
+{
+    public static void Main()
+    {
+        double a, b, temp;
+        Console.WriteLine("Enter the value of a:");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the value of b:");
+        b = double.Parse(Console.ReadLine());
+        if (a > b)
+        {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+        Console.WriteLine("a = {0}, b = {1}", a, b);
+    }
+}
+
+// output:
+//    a = 1, b = 2  same.
+//    a = 2, b = 1  switch.
+//    a = 1, b = 1  same.
+// 56 / 5,000
+// The purpose of this program is to switch the variables a and b if a is greater than b.
---
+++ 27.09.22-72,18/27.09.22-72,18/Program.cs
@@ -0,0 +1,24 @@
+﻿class Assgnment_18
+{
+    public static void Main()
+    {
+        Console.WriteLine("Program A");
+        Console.WriteLine("Enter a number:");
+        int num = int.Parse(Console.ReadLine());
+        if (num < 0)
+            num = num * (-1);
+        Console.WriteLine(num);
+
+        Console.WriteLine("Program b");
+        if (num < 0)
+            num = num * (-1);
+        else
+            Console.WriteLine(num);
+    }
+}
+
+
+//output:
+//    num = 5  te number 5 in both of them.
+//    The purpose of program A is to do an absolute value for every number that is less than zero.
+//    The difference between the programs is that program B will not print a number that is less than zero.
